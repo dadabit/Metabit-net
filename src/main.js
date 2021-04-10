3839999a53c378121801b9fbe7fb9db72a46d3bd
dadabit <sikai.dada@gmail.com>
2021-04-10 19:57:35 +0800

    dddd

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-alert("hello world")
+let engine = Matter.Engine.create();
+
+let render = Matter.Render.create({
+    element: document.body,
+    engine: engine
+});
+
+let ground = Matter.Bodies.rectangle(200, 300, 500, 350, { isStatic: true });
