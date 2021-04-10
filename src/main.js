c0887b78d1677558732aebebd20061c05c9ac890
dadabit <sikai.dada@gmail.com>
2021-04-10 20:00:39 +0800

    jj

--- src/main.js
+++ src/main.js
@@ -6,3 +6,8 @@ let render = Matter.Render.create({
 });
 
 let ground = Matter.Bodies.rectangle(200, 300, 500, 350, { isStatic: true });
+let boxA = Matter.Bodies.rectangle(100, 100, 80, 80);
+let boxb = Matter.Bodies.rectangle(100, 200, 80, 80);
+
+Matter.World.add(engine.world, [boxA, boxB, ground]);
+Matter.Engine.run(engine);
