cb2fc6a74002db530461bc0dcca7a2ee5f9f3315
sikai <dada@sikais-Mac-mini.local>
2021-05-26 15:03:27 +0800

    added

--- lodle.js/main.js
+++ lodle.js/main.js
@@ -1,20 +1,32 @@
 
-var agents = [];
-
+// Create a canvas of given size
+let angle = 0;
+let depth = 0;
+  
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(0);
-	frameRate(10);
-
-	for (i = 0; i < 50; i++) {
-		agents.push(new Agent(random(0, windowWidth), random(0, windowHeight)));
-	} 
+    background(0);
+    frameRate(240);
+    createCanvas(windowWidth, windowHeight, WEBGL);
 }
-
+  
+// Create a draw function
 function draw() {
-	// background(0);
-	for (key of agents) {
-		key.update();
-		key.show();
-	}
+
+    // depth -= random(-10, 10);
+    
+    ambientLight(255);
+    push();
+    
+    translate(0, 0, depth);
+              
+    rotateX(angle * 0.5);
+    rotateZ(angle * 0.7);
+    rotateY(angle * 0.06);
+    stroke(0, 0, 0);
+    normalMaterial();
+  
+    // Create box of given size
+    box(500, 500, 10);
+    push();
+    angle += .06
 }
