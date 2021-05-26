e6bfef08e638ed82cb737eea243703acc2fa30fa
dadabit <sikai.dada@gmail.com>
2021-05-26 16:55:05 +0800

    commit

--- auxandmics/main.js
+++ auxandmics/main.js
@@ -1,7 +1,27 @@
+cells = [];
+population = 70;
+
 function setup() {
+    createCanvas(800, 800, P2D)
+    background(0);
+    
+    for (var i = 0; i < population; i++)
+    {
+        cells.push(new Agent(random(width/2-70, width/2), random(height/2-15, height/2), random(-1, 1)));
+    }
+
+    frameRate(240)
 
 }
 
 function draw() {
+    // background(0)
+
+    for (cell of cells) {
+        cell.update();
+        cell.show();
+        cell.dir += 0.1*frameCount*cell.factor;
+        cell.speed = 10;
+    }
 
 }
